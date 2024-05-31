730fc02d693cc3eaa2532572ddeed685b88bdf25
Matthew Tran <tran28@my.yorku.ca>
2024-05-31 14:26:48 -0400

    UPDATE: component file naming to .jsx, add lenis scroll

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './App';
 import './index.css';
+import { ReactLenis } from '@studio-freight/react-lenis';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ReactLenis root>
+      <App />
+    </ReactLenis>
   </React.StrictMode>
 );
--- src/utils/smoothScroll.js
+++
@@ -1,8 +0,0 @@
-const smoothScroll = (targetId) => {
-    const targetSection = document.getElementById(targetId);
-    if (targetSection) {
-        targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-};
-
-export default smoothScroll;
