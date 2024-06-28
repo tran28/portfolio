e5afa44ab883e5c446e42ab97b1cbc65b256de02
Matthew Tran <matthewtran.jobs@gmail.com>
2024-06-28 12:37:25 -0400

    Update accolades.js

--- src/content/accolades.js
+++ src/content/accolades.js
@@ -2,7 +2,7 @@ export const accolades = [
     {
         image: "https://matthew-tran-portfolio.s3.us-east-2.amazonaws.com/BEST_award.png",
         title: "1st Place, Venture Course",
-        href: "https://www.linkedin.com/in/matthewtran-eng/overlay/experience/1614664479/multiple-media-viewer/?profileId=ACoAACv4KhQBLhLr7rVNt8iK6Rwsykbn7v65WlE&treasuryMediaId=1588498683495",
+        href: "https://matthew-tran-portfolio.s3.us-east-2.amazonaws.com/image_heritude.jpeg",
         description: "Awarded to the first-place team participating in the Entrepreneurship & Technology Ventures course at the Schulich School of Business. Built MVP and designed three pitch decks using Powerpoint, Figma, Illustrator, Webflow.",
     },
     {
