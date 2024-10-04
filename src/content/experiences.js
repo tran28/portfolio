34f22624b061fe18467a323f1d066f697e7dfcf9
Matthew Tran <matthewtran.jobs@gmail.com>
2024-10-04 10:23:09 -0400

    Update experiences.js

--- src/content/experiences.js
+++ src/content/experiences.js
@@ -1,47 +1,56 @@
 export const experiences = [
-    {
-        dateSpan: "Jan 2023 - Present",
-        titles: ["Educator", "Web Development Mentor"],
-        company: "Lighthouse Labs",
-        href: "https://www.lighthouselabs.ca/",
-        description: "Provide learning instructions to 500+ students in modern web development technologies; topics covering fundamentals of Javascript, Python, HTML, CSS, React, Next.js, Node.js, PostgreSQL, Git, and automated testing (e.g. Mocha Chai, Cypress)",
-        skills: ["Web Development", "Content Delivery"],
-        technologies: ["Javascript", "React", "Express", "SQL", "Git"],
-    },
-    {
-        dateSpan: "Oct 2023 - Feb 2024",
-        titles: ["Software Engineer"],
-        company: "Susgrainable",
-        href: "https://susgrainable.com/",
-        description: "Collaborated with associate developer to optimize Shopify site with emphasis on responsive design and documenting legacy code.",
-        skills: ["System Design"],
-        technologies: ["Shopify Liquid", "HTML", "CSS"],
-    },
-    {
-        dateSpan: "Mar 2023 - Aug 2023",
-        titles: ["Developer", "Undergraduate Program Secretary"],
-        company: "York University",
-        href: "https://www.yorku.ca/science/chemistry/",
-        description: "Implemented automation tools based on administrative needs of the Department of Chemistry.",
-        skills: ["Scripting", "Task Automation"],
-        technologies: ["Javascript", "Python", "Selenium"],
-    },
-    {
-        dateSpan: "Aug 2021 - Apr 2023",
-        titles: ["Co-founder"],
-        company: "Heritude Inc.",
-        href: "https://yfile.news.yorku.ca/2020/04/29/startup-focused-on-supporting-indigenous-artisans-wins-pitch-competition-coveted-spot-in-bestlab/",
-        description: "Successfully launched first product offering in 2021 after conducting in-depth market and risk analysis, product positioning, and planning requirements; achieving break-even in 14 months.",
-        skills: ["Startup", "Business Management"],
-        technologies: ["Shopify", "Amazon FBA"],
-    },
-    {
-        dateSpan: "Sep 2019 - Dec 2022",
-        titles: ["Lead Site Coordinator", "Operations Coordinator", "Finance Coordinator"],
-        company: "Let's Talk Science",
-        href: "https://letstalkscience.ca/",
-        description: "Wrote 50+ bug reports for the newly developed in-house volunteer portal. Led a team of three new coordinators and fulfilled duties to stakeholders.",
-        skills: ["Leadership", "Project Management", "Documentation"],
-        technologies: ["Google Suite", "MailChimp Automation"],
-    },
-]
+  {
+    dateSpan: "Sep 2024 - Present",
+    titles: ["Software Engineer"],
+    company: "Postilize",
+    href: "https://www.postilize.com/",
+    description:
+      "Develop and test features for startup aiming to automate tasks for law, accounting, and consulting firms.",
+    skills: ["System Design", "Software Testing"],
+    technologies: ["Javascript", "React", "Express", "MongoDB"],
+  },
+  {
+    dateSpan: "Jan 2023 - Present",
+    titles: ["Educator", "Web Development Mentor"],
+    company: "Lighthouse Labs",
+    href: "https://www.lighthouselabs.ca/",
+    description:
+      "Provide learning instructions to 500+ students in modern web development technologies; topics covering fundamentals of Javascript, Python, HTML, CSS, React, Next.js, Node.js, PostgreSQL, Git, and automated testing (e.g. Mocha Chai, Cypress)",
+    skills: ["Web Development", "Content Delivery"],
+    technologies: ["Javascript", "React", "Express", "SQL", "Git"],
+  },
+  {
+    dateSpan: "Mar 2023 - Aug 2023",
+    titles: ["Developer", "Undergraduate Program Secretary"],
+    company: "York University",
+    href: "https://www.yorku.ca/science/chemistry/",
+    description:
+      "Implemented automation tools based on administrative needs of the Department of Chemistry.",
+    skills: ["Scripting", "Task Automation"],
+    technologies: ["Javascript", "Python", "Selenium"],
+  },
+  {
+    dateSpan: "Aug 2021 - Apr 2023",
+    titles: ["Co-founder"],
+    company: "Heritude Inc.",
+    href: "https://yfile.news.yorku.ca/2020/04/29/startup-focused-on-supporting-indigenous-artisans-wins-pitch-competition-coveted-spot-in-bestlab/",
+    description:
+      "Successfully launched first product offering in 2021 after conducting in-depth market and risk analysis, product positioning, and planning requirements; achieving break-even in 14 months.",
+    skills: ["Startup", "Business Management"],
+    technologies: ["Shopify", "Amazon FBA"],
+  },
+  {
+    dateSpan: "Sep 2019 - Dec 2022",
+    titles: [
+      "Lead Site Coordinator",
+      "Operations Coordinator",
+      "Finance Coordinator",
+    ],
+    company: "Let's Talk Science",
+    href: "https://letstalkscience.ca/",
+    description:
+      "Wrote 50+ bug reports for the newly developed in-house volunteer portal. Led a team of three new coordinators and fulfilled duties to stakeholders.",
+    skills: ["Leadership", "Project Management", "Documentation"],
+    technologies: ["Google Suite", "MailChimp Automation"],
+  },
+];
